f69e59be131418114322d23aa0e1576a2cb3b6cd
Bertie <berniesnell@gmail.com>
2019-06-05 15:06:32 -0400

    change path of xor

--- set1/single-byte-xor-cipher/src/main.rs
+++ set1/single-byte-xor-cipher/src/main.rs
@@ -15,7 +15,7 @@ use xor::xor;
 
 fn main() {
     let hex_encoded_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    xor(hex_encoded_string, "");
+    xor(hex_encoded_string, "0");
 
     println!("Hello, world!");
 }
