ecc4d9841749a1ad9788689abffc2abf3b6e3109
Bertie <berniesnell@gmail.com>
2019-06-07 13:29:51 -0400

    refactor to return hashmap of keysizes

--- set1/break_repeating_xor/src/main.rs
+++ set1/break_repeating_xor/src/main.rs
@@ -12,18 +12,21 @@ fn main() {
 
 mod break_repeating_xor {
     use single_byte_xor_cipher::cipher::cipher::xor;
+    use std::collections::HashMap;
 
-    pub fn find_smallest_key(text: &str) -> (i32, i32) { // EDIT: actually want to capture smallest 3 values
+    pub fn find_smallest_key(text: &str) -> HashMap<i32, Vec<i32>> { // normalised distance mapped to keysizes
         generate_keysizes()
             .iter()
-            .fold((900, 0), |acc, key| { // hamming distance, then keysize // use hashmap?
-                let bytes = get_first_two_keysizes_of_bytes(text, *key);
+            .fold(HashMap::new(), |mut acc, keysize| {
+                let bytes = get_first_two_keysizes_of_bytes(text, *keysize);
                 let hamming_distance = calculate_hamming_distance(bytes.0, bytes.1);
-                let normalised_hamming_distance = normalise_distance(hamming_distance, *key);
-                println!("Keysize testing: {:?}, Hamming distance calculated at: {:?}, normalised to {:?}", *key, hamming_distance, normalised_hamming_distance);
-                if (hamming_distance) < acc.0 {
-                    return (hamming_distance, *key);
-                }
+                let normalised_hamming_distance = normalise_distance(hamming_distance, *keysize);
+
+                let current_vec = acc.entry(normalised_hamming_distance).or_insert(vec!());
+                let mut updated_vec = current_vec.clone(); 
+                updated_vec.push(*keysize);
+                *current_vec = updated_vec;
+        
                 acc
             })
     }
