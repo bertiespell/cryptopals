2e010455691ed4ef4680db816287e8f34164e5e0
Bertie <berniesnell@gmail.com>
2019-06-12 11:47:08 -0400

    refactor and add tests

--- set1/detect_aes/src/main.rs
+++ set1/detect_aes/src/main.rs
@@ -20,9 +20,12 @@ fn main() {
         .split("\n")
         .collect::<Vec<&str>>();
 
-    // the same 16 byte plaintext block will always produce the same 16 byte ciphertext. i.e. common 16 byte plain text block - repeats commonly...
+    let found = detect_aes(ciphertexts);
+    println!("Found: {:?}", found);
+}
 
-    let mut needles = ciphertexts
+fn detect_aes(ciphertexts: Vec<&str>) -> String {
+        let mut needles = ciphertexts
         .iter()
         .map(|text| {
             text
@@ -50,7 +53,24 @@ fn main() {
     needles
         .sort_by(|a, b| b.0.values().fold(0, add_scores_above_1).cmp(&a.0.values().fold(0, add_scores_above_1)));
 
-    println!("Found needle: {:?}", needles[0]);
+    String::from(needles[0].1.clone())
 }
 
-//d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_xor() {
+        let hex_encoded_data = fs::read_to_string("text.txt")
+        .expect("Unable to read file");
+        let ciphertexts = hex_encoded_data
+            .split("\n")
+            .collect::<Vec<&str>>();
+        let result = detect_aes(ciphertexts);
+        let actual = "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a";
+
+        assert_eq!(result, actual);
+    }
+}
