fea2d17ed4edfd481f5a211b53435e15c09256ae
Bertie <berniesnell@gmail.com>
2019-06-12 13:47:14 -0400

    add tests and split into proper modular pattern

---
+++ set1/detect_single_xor/src/lib.rs
@@ -0,0 +1,48 @@
+extern crate single_byte_xor_cipher;
+use hex;
+use single_byte_xor_cipher::cipher::cipher::decrypt;
+use single_byte_xor_cipher::cipher::cipher::score_xored_hashes;
+
+pub struct ScoredXorStrings {
+    pub score: i32,
+    pub encrypted_strings: String
+}
+
+impl ScoredXorStrings {
+    fn new() -> ScoredXorStrings {
+        ScoredXorStrings {
+            score: 0,
+            encrypted_strings: String::new()
+        }
+    }
+}
+
+pub fn detect_single_xor(data: String) -> ScoredXorStrings {
+    data
+        .lines()
+        .map(|line| decrypt(hex::decode(line).unwrap()))
+        .fold(ScoredXorStrings::new(), |acc, decrypted_line| {
+            let score = score_xored_hashes(decrypted_line.decoded.as_bytes().to_vec());
+            if score > acc.score {
+                return ScoredXorStrings {
+                    score,
+                    encrypted_strings: decrypted_line.decoded
+                };
+            }
+            acc
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_detection() {
+        let data = fs::read_to_string("encrypted_strings.txt").expect("Unable to read file");
+        let decrypted = detect_single_xor(data);
+        let actual = "Now that the party is jumping\n";
+        assert_eq!(actual, &decrypted.encrypted_strings);
+    }
+}
--- set1/detect_single_xor/src/main.rs
+++ set1/detect_single_xor/src/main.rs
@@ -1,37 +1,8 @@
-extern crate single_byte_xor_cipher;
+use detect_single_xor;
 use std::fs;
 
-use single_byte_xor_cipher::cipher::cipher::decrypt;
-use single_byte_xor_cipher::cipher::cipher::score_xored_hashes;
-
-struct ScoredXorStrings {
-    pub score: i32,
-    pub encrypted_strings: String
-}
-
-impl ScoredXorStrings {
-    fn new() -> ScoredXorStrings {
-        ScoredXorStrings {
-            score: 0,
-            encrypted_strings: String::new()
-        }
-    }
-}
 fn main() {
     let data = fs::read_to_string("encrypted_strings.txt").expect("Unable to read file");
-    let decrypted = data
-        .lines()
-        .map(|line| decrypt(line.as_bytes().to_vec()))
-        .fold(ScoredXorStrings::new(), |acc, decrypted_line| {
-            let score = score_xored_hashes(decrypted_line.decoded.as_bytes().to_vec());
-            if score > acc.score {
-                return ScoredXorStrings {
-                    score,
-                    encrypted_strings: decrypted_line.decoded
-                };
-            }
-            acc
-        });
-
-    println!("{:?}", decrypt(decrypted.encrypted_strings.as_bytes().to_vec()).decoded);
+    let decrypted = detect_single_xor::detect_single_xor(data);
+    println!("{:?}", &decrypted.encrypted_strings);
 }
