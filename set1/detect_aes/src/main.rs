ac341615a1f6e90a21b3e211022783f04284b527
Bertie <berniesnell@gmail.com>
2019-06-12 11:41:36 -0400

    solution to challenge 8

--- set1/detect_aes/src/main.rs
+++ set1/detect_aes/src/main.rs
@@ -11,36 +11,46 @@ Remember that the problem with ECB is that it is stateless and deterministic; th
 extern crate hex;
 use std::fs;
 use std::collections::HashMap;
+use std::str;
 
 fn main() {
-    let hex_encoded_data = fs::read_to_string("text.txt").expect("Unable to read file");
+    let hex_encoded_data = fs::read_to_string("text.txt")
+        .expect("Unable to read file");
     let ciphertexts = hex_encoded_data
         .split("\n")
-        .map(|text| hex::decode(text).unwrap())
-        .collect::<Vec<_>>();
+        .collect::<Vec<&str>>();
 
-    // the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
+    // the same 16 byte plaintext block will always produce the same 16 byte ciphertext. i.e. common 16 byte plain text block - repeats commonly...
 
-    // common 16 byte plain text block - repeats commonly...
-    let mut needle = ciphertexts
+    let mut needles = ciphertexts
         .iter()
         .map(|text| {
             text
+                .as_bytes()
                 .chunks(16)
-                .fold(HashMap::new(), |mut acc, chunk| {
-                    let chunk_entry = acc
-                        .entry(chunk)
+                .map(str::from_utf8)
+                .fold((HashMap::new(), String::from(*text)), |mut acc, chunk| {
+                    let chunk_entry = acc.0
+                        .entry(chunk.unwrap())
                         .or_insert(0);
+                    
                     *chunk_entry += 1;
                     acc
                 })
         })
-        .collect::<Vec<HashMap<_, _>>>();
-    
-    needle
-        .sort_by(|a, b| {
-            a.values().fold(0, |acc, entry| acc + entry).cmp(&b.values().fold(0, |acc, entry| acc + entry))
-        });
-        
-    println!("Needle: {:?}", needle[0]);
+        .collect::<Vec<_>>();
+
+    fn add_scores_above_1(acc: i32, entry: &i32) -> i32 {
+        if *entry > 1 {
+            return acc + *entry;
+        }
+        acc
+    }
+
+    needles
+        .sort_by(|a, b| b.0.values().fold(0, add_scores_above_1).cmp(&a.0.values().fold(0, add_scores_above_1)));
+
+    println!("Found needle: {:?}", needles[0]);
 }
+
+//d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a
