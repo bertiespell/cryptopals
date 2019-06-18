3d9ca638fd4aa2d02b0a7cedce8a004fa1d6c5f6
Bertie <berniesnell@gmail.com>
2019-06-18 11:13:51 -0400

    decrypt file

--- set2/cbc_mode/src/lib.rs
+++ set2/cbc_mode/src/lib.rs
@@ -13,8 +13,10 @@ The file here is intelligible (somewhat) when CBC decrypted against "YELLOW SUBM
 Don't cheat.
 Do not use OpenSSL's CBC code to do CBC mode, even to verify your results. What's the point of even doing this stuff if you aren't going to learn from it?
  */
-use std::fs;
 use pkcs_padding;
+use aes_in_ecb;
+use single_byte_xor_cipher;
+use decrypt_repeating::base_64;
 
 pub fn decrypt(encrypted_text: Vec<u8>, key: Vec<u8>, IV: Vec<u8>) -> Vec<u8> {
     // Implement CBC mode by hand by taking the ECB function you wrote earlier, making it encrypt instead of decrypt (verify this by decrypting whatever you encrypt to test), and using your XOR function from the previous exercise to combine them.
@@ -24,22 +26,45 @@ pub fn decrypt(encrypted_text: Vec<u8>, key: Vec<u8>, IV: Vec<u8>) -> Vec<u8> {
     // Decrypt next block (XOR with previous ciphertext)
     let mut last_block = IV;
 
-    encrypted_text
+    let base_64_decoded = base_64::decode(&encrypted_text);
+    let mut string_result = String::new();
+
+    base_64_decoded
         .chunks(key.len()) // each block should be the length of the key
-        .for_each(|entry| {
-            let padded_entry = pkcs_padding::pad_to_bytes(entry.to_vec(), key.len()); // these are all now the length of the key
-            // decrypt block
-            // xor against last_block
-            // update last block (last_block = entry;)
-            dbg!(padded_entry.len());
+        .enumerate()
+        .for_each(|(index, entry)| {
+            // if index == 0 {
+                let padded_entry = pkcs_padding::pad_to_bytes(entry.to_vec(), key.len()); // these are all now the length of the key
+                // decrypt block
+                // xor against last_block
+
+                let decrypted_block = aes_in_ecb::decrypt_data(
+                    padded_entry, 
+                    &key, 
+                    last_block.clone()
+                ).unwrap();
+
+                let xor_result = single_byte_xor_cipher::xor(
+                    decrypted_block.clone()[0..16].to_vec(), 
+                    last_block.clone()
+                );
+                let my_string = String::from_utf8(xor_result).unwrap();
+                string_result.push_str(&my_string);
+                // update last block (last_block = entry;)
+                last_block = entry.to_vec();
+            // }
         })
     ;
+
+                    println!("{}", string_result);
+
     vec!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn test() {
--- set2/cbc_mode/src/main.rs
+++ set2/cbc_mode/src/main.rs
@@ -4,12 +4,13 @@ use cbc_mode;
 fn main() {
     let key = "YELLOW SUBMARINE";
     let IV =  "\x00\x00\x00";
-    let IV_lengthened = pkcs_padding::pad_with_str(IV, key.as_bytes().len(), 0 as u8);
+    let IV_lengthened = pkcs_padding::pad_with_bytes(IV.as_bytes().to_owned(), key.as_bytes().len(), 0 as u8);
     let encypted_text = fs::read_to_string("encrypted_data.txt").expect("Unable to read file");
+    // dbg!(encypted_text.as_bytes().to_owned());
     let decrypted_bytes = cbc_mode::decrypt(
         encypted_text.as_bytes().to_owned(), 
         key.as_bytes().to_owned(), 
-        IV_lengthened.as_bytes().to_owned()
+        IV_lengthened
     );
     let decrypted_text = String::from_utf8(decrypted_bytes).unwrap();
     println!("Decrypted: {:?}", decrypted_text);
